4f65ce177f7ccf2a5c00c41f46e1096241189d0a
Ismael Strey <imsaletsrey@hotmail.com>
2020-01-02 17:47:29 -0300

    Configuração do MD com graphql

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,19 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const { createFilePath } = require('gatsby-source-filesystem')
+// to add slug field to each post
+exports.onCreateNode = ({ node, actions, getNode }) => {
+  const { createNodeField } = actions
 
-// You can delete this file if you're not using it
+  if (node.internal.type === 'MarkdownRemark') {
+    const slug = createFilePath({
+      node,
+      getNode,
+      basePath: 'pages'
+    })
+
+    createNodeField({
+      node,
+      name: 'slug',
+      value: `/${slug}`
+    })
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,20 +2,63 @@ import React from 'react'
 import Layout from '../components/Layout'
 import SEO from '../components/seo'
 import PostItem from '../components/PostItem'
+import { useStaticQuery, graphql } from 'gatsby'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <PostItem
-      slug="/about/"
-      background="tomato"
-      category="Misc"
-      date="30 de julho de 2019"
-      timeToRead="5"
-      title="Diga não ao Midium: Tenha sua propria plataforma"
-      description="Culpa veniam enim incididunt do qui adipisicing nulla consequat deserunt ex."
-    />
-  </Layout>
-)
+const IndexPage = () => {
+  const { allMarkdownRemark } = useStaticQuery(
+    graphql`
+      query PostList {
+        allMarkdownRemark {
+          edges {
+            node {
+              id
+              fields {
+                slug
+              }
+              frontmatter {
+                background
+                category
+                description
+                title
+                date(
+                  difference: ""
+                  locale: "pt-br"
+                  formatString: "DD [de] MMMM [de] YYYY"
+                )
+              }
+              timeToRead
+            }
+          }
+        }
+      }
+    `
+  )
+  const postList = allMarkdownRemark.edges
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {postList.map(
+        ({
+          node: {
+            frontmatter: { background, category, date, description, title },
+            timeToRead,
+            id,
+            fields: { slug }
+          }
+        }) => (
+          <PostItem
+            slug={slug}
+            background={background}
+            category={category}
+            date={date}
+            timeToRead={timeToRead}
+            title={title}
+            description={description}
+          />
+        )
+      )}
+    </Layout>
+  )
+}
 
 export default IndexPage
